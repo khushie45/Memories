ae26b5b4d2cc6ef17877b40e3867e01e5209bf93
Khushi Patel <khushipatel3340@gmail.com>
2023-09-04 23:41:08 +0530

    Commit

--- client/src/components/Posts/AllPosts.jsx
+++ client/src/components/Posts/AllPosts.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import Post from './Post/Post'
-import useStyles from './styles'
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
-function AllPosts() {
-  const classes = useStyles()
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const AllPosts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
   return (
-    <>
-      <h1>AllPosts</h1>
-      <Post />
-      <Post />
-    </>
-  )
-}
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
 
-export default AllPosts
+export default AllPosts;
--- server/index.js
+++ server/index.js
@@ -6,13 +6,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://khushie45:khushie45@cluster0.ycdqo0t.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://khushie45:khushie45@cluster0.aijryvr.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL)
